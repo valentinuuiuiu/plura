bd392e39fa2bc44adda647e9da04a97b3d2a7a70
valentinuuiuiu <105746659+valentinuuiuiu@users.noreply.github.com>
2024-04-18 20:56:36 +0100

    Initial commit

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+
+export default authMiddleware({
+    publicRoutes: ["/site", "/api/uploadthing"],
+})
+
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/",
+"/(api|trpc)(.*)"],
+}
