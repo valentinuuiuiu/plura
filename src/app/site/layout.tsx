0eab899c0d20eab17424f0edda3f91a4386e5c9a
valentinuuiuiu <105746659+valentinuuiuiu@users.noreply.github.com>
2024-05-07 22:48:30 +0100

    comm
    second

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,12 +1,17 @@
 import Navigation from '@/components/site/navigation'
 import React from 'react'
+import { dark } from '@clerk/themes'
+import { ClerkProvider } from '@clerk/nextjs'
 
 
 const layout = ({children}:{children: React.ReactNode}) => {
   return (
-    <main className='h-full'>
+    <ClerkProvider appearance={{baseTheme:dark}}>
+ <main className='h-full'>
         <Navigation />
         {children}</main>
+    </ClerkProvider>
+   
   )
 }
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,44 @@
 import { authMiddleware } from "@clerk/nextjs";
-
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-    publicRoutes: ["/site", "/api/uploadthing"],
-})
+  publicRoutes: ["/site", "/api/uploadthing"],
+  async beforeAuth(auth, req) {},
+  async afterAuth(auth, req) {
+    //rewrite for domains
+    const url = req.nextUrl;
+    const searchParams = url.searchParams.toString();
+    let hostname = req.headers;
+
+    const pathWithSearchParms = `${url.pathname}${
+      searchParams.length > 0 ? `?${searchParams}` : ""
+    }`;
+    //if subdomain exists
+    const customSubDomain = hostname
+      .get("host")
+      ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+      .filter(Boolean)[0];
+
+    if (customSubDomain) {
+      return NextResponse.rewrite(
+        new URL(`/${customSubDomain}${pathWithSearchParms}`,req.url)
+      );
+    }
+
+    if (url.pathname === "/sign-in" || url.pathname === "/sign-up") {
+      return NextResponse.redirect(new URL(`/agency/sign-in`, req.url));
+    }
+
+    if(url.pathname==='/' || url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN){
+        return NextResponse.rewrite(new URL('/site', req.url))
+    }
+
+    if(url.pathname.startsWith('/agency') ||url.pathname.startsWith('/subaccount')){
+        return NextResponse.rewrite(new URL(`/${customSubDomain}${pathWithSearchParms}`, `http://${process.env.NEXT_PUBLIC_DOMAIN}`))
+    }
+  },
+});
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/",
-"/(api|trpc)(.*)"],
-}
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
